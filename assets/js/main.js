d9d70d08be05ce5bb35e72afa1bd917ef3b061c4
Bernardo Veras <bernardo@vvssistemas.com.br>
2022-01-19 16:02:49 -0300

    Add fetch

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,10 +27,13 @@ document.addEventListener('click', e => {
 async function loadPage(element) {
   try {
     const href = element.getAttribute('href');
+    const response = await fetch(href);
+
+    if(response.status !== 200) throw new Error('ERRO 404!');
+
+    const html = await response.text();
   
-    const response = await request({method: 'GET', url: href});
-  
-    loadResult(response);
+    loadResult(html);
   } catch (error) {
     handleError(error);
   }
